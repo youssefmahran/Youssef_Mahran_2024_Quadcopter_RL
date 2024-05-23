cc62b9ba3a8aed215a358065b00ab324c6f7f70b
youssefmahran <107947332+youssefmahran@users.noreply.github.com>
2024-05-24 01:48:30 +0200

    Update TrackingResult.py

--- gym-pybullet-drones/gym_pybullet_drones/Simulation/TrackingResult.py
+++ gym-pybullet-drones/gym_pybullet_drones/Simulation/TrackingResult.py
@@ -2,7 +2,7 @@ from stable_baselines3 import SAC
 import time
 
 import sys
-sys.path.append('/home/youssefmahran2/Desktop/Youssef_Mahran_2024_Quadcopter_RL/gym-pybullet-drones')
+sys.path.append('')
 
 from gym_pybullet_drones.envs.BaseAviary import DroneModel, Physics
 from gym_pybullet_drones.envs.BaseRLAviary import ActionType, ObservationType
